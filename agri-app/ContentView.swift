881e396365adda8bb1aff35d8e968fbf4622cee3
bipin7giri <bipingiri27@gmail.com>
2024-03-21 21:58:34 +0545

    Initial Commit

---
+++ agri-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  agri-app
+//
+//  Created by Bipin Giri on 21/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ agri-app/agri_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  agri_appApp.swift
+//  agri-app
+//
+//  Created by Bipin Giri on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct agri_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
